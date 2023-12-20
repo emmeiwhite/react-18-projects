5c94c0677487186a5d3bdb77e056f1c92fc9b24b
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-12-20 16:24:02 +0530

    repeat(auto-fill, minmax(200px, 1fr))

--- 09-color-generator/starter/src/components/Shades.jsx
+++ 09-color-generator/starter/src/components/Shades.jsx
@@ -25,7 +25,7 @@ export default function Shades({ colors }) {
       style={{
         height: "500px",
         display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+        gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
       }}
     >
       {/* Render the shades */}
